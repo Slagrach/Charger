b92cd68f489f4573ed9c4524793f20acad17aa80
slagrach <slagrach@gmail.com>
2020-10-21 23:32:46 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slider-items').slick({
+    arrows: false,
+    slidesToScroll: 3,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    // centerMode: true,
+    variableWidth: true,
+    infinite: true
+});
